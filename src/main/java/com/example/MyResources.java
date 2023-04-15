229d7affbf6f92d06cad7c147487316fcebe77f0
sosuisen <hidekazu.kubota@gmail.com>
2023-04-16 01:23:41 +0900

    feat: serialize messages to json

--- src/main/java/com/example/MyResources.java
+++ src/main/java/com/example/MyResources.java
@@ -1,8 +1,15 @@
 package com.example;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -14,13 +21,68 @@ import org.glassfish.jersey.server.mvc.Viewable;
 import com.example.model.MessageDTO;
 import com.example.model.Messages;
 
+@ApplicationScoped
 @Path("/")
 public class MyResources {
+	// データの保存先を適切なパスへ変更してください。
+	private String saveFilePath = "c:\\pleiades-ssj2023\\data.json";
+
+	/**
+	 * MyResourcesがインスタンス化された後、
+	 * この@Injectアノテーションは、
+	 * Messages.javaで宣言されたCDI管理Beanである
+	 * Messagesクラスを自動的にインスタンス化して、messagesへインジェクションします。
+	 */
 	@Inject
 	private Messages messages;
 
+	/**
+	 * 上のインスタンスが生成された直後に実行する処理です。
+	 * MessagesクラスはApplication　Scopeであるため、
+	 * 生成されるのはアプリが起動したときです。
+	 * saveFilePathからJSON文字列を読み込んで、
+	 * Messagesオブジェクトへ変換し、messagesへセットしています。
+	 */
+	@PostConstruct
+	public void prepare() {
+		Jsonb jsonb = JsonbBuilder.create();
+		try {
+			String json = Files.readString(java.nio.file.Path.of(saveFilePath));
+			messages.addAll(jsonb.fromJson(json, Messages.class));
+		} catch (IOException err) {
+			err.printStackTrace();
+		}
+	}
+
+	/**
+	 * 上のインスタンスが破壊される直前に実行する処理です。
+	 * MessagesクラスはApplication　Scopeであるため、
+	 * 破壊されるのはアプリが終了するときです。
+	 * messagesの内容をJSON文字列へ変換して、saveFilePathへ保存しています。
+	 */
+	@PreDestroy
+	public void after() {
+		Jsonb jsonb = JsonbBuilder.create();
+		String json = jsonb.toJson(messages);
+		try {
+			Files.writeString(java.nio.file.Path.of(saveFilePath), json);
+		} catch (IOException err) {
+			err.printStackTrace();
+		}
+	}
+
 	private String userName = "KCG";
 
+	/**
+	 * @Path("")は、
+	 * http://localhost:8080/MessageMVC/message
+	 * または
+	 * http://localhost:8080/MessageMVC/message/
+	 * にマッチします。
+	 * 今回の index.jsp　は、
+	 * http://localhost:8080/MessageMVC/message/
+	 * を開かないと、リンクが正しく動作しないので注意。
+	 */
 	@GET
 	@Path("")
 	public Viewable home() {
